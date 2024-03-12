2a90c64fe7b1842a97a89303d27b97a8a1c6de53
akemilol <valeriaconcedsantos@gmail.com>
2024-03-11 23:27:14 -0300

    primeiro commit

---
+++ aula04/dados.py
@@ -0,0 +1,7 @@
+campus = 'FIAP Paulista'
+curso = 'Analise e Desenvolvimento de Sistemas'
+nomeCompleto = 'Valéria Conceição Dos santos'
+rm = 123456
+email = 'alunofiap@gmail.com'
+
+print("\n", campus, "\n", curso, "\n", nomeCompleto, "\n", rm, "\n", email)
---
+++ aula04/somar.py
@@ -0,0 +1,4 @@
+numero1 = int(input('Digite um valor:'))
+numero2 = int(input('Digite um outro número:'))
+resultado = numero1 + numero2
+print('A soma entre {} e {} é igual a {}!'.format(numero1, numero1, resultado))
